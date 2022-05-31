77cc79d12d6782722c5b26acaa2496d9c3ba2cc6
Emmanuelle MAPELLI <emmanuellemapelli@gmail.com>
2022-05-31 16:33:04 +0200

    tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_31_094103) do
+ActiveRecord::Schema.define(version: 2022_05_31_095016) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
-    t.integer "rating"
+    t.string "title"
+    t.text "details"
+    t.boolean "completed", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
